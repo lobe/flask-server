c31e1c76237b5f56f8c58080e6ebb40d76b175f1
Hannah Krager <hakrager@microsoft.com>
2021-03-07 19:26:24 -0800

    update code outputs

--- tf_model_helper.py
+++ tf_model_helper.py
@@ -4,61 +4,41 @@
 """
 Skeleton code showing how to load and run the TensorFlow SavedModel export package from Lobe.
 """
+import argparse
 import os
 import json
 import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-class TFModel(object):
-    def __init__(self, model_dir):
-        # make sure our exported SavedModel folder exists
-        model_path = os.path.realpath(model_dir)
-        if not os.path.exists(model_path):
-            raise ValueError(f"Exported model folder doesn't exist {model_dir}")
-        self.model_path = model_path
 
-        # load our signature json file, this shows us the model inputs and outputs
-        # you should open this file and take a look at the inputs/outputs to see their data types, shapes, and names
-        with open(os.path.join(model_path, "signature.json"), "r") as f:
+class TFModel:
+    def __init__(self, model_dir) -> None:
+        # make sure our exported SavedModel folder exists
+        self.model_dir = model_dir
+        with open(os.path.join(model_dir, "signature.json"), "r") as f:
             self.signature = json.load(f)
+        self.model_file = "../" + self.signature.get("filename")
+        if not os.path.isfile(self.model_file):
+            raise FileNotFoundError(f"Model file does not exist")
         self.inputs = self.signature.get("inputs")
         self.outputs = self.signature.get("outputs")
-        self.labels = self.signature.get("classes").get("Label")
-
         # placeholder for the tensorflow session
         self.session = None
 
-    def load(self):
+    def load(self) -> None:
         self.cleanup()
         # create a new tensorflow session
         self.session = tf.compat.v1.Session(graph=tf.Graph())
         # load our model into the session
-        tf.compat.v1.saved_model.loader.load(sess=self.session, tags=self.signature.get("tags"), export_dir=self.model_path)
+        tf.compat.v1.saved_model.loader.load(sess=self.session, tags=self.signature.get("tags"), export_dir=self.model_dir)
 
-    def predict(self, image: Image.Image):
+    def predict(self, image: Image.Image) -> dict:
         # load the model if we don't have a session
         if self.session is None:
             self.load()
-        # get the image width and height
-        width, height = image.size
-        # center crop image (you can substitute any other method to make a square image, such as just resizing or padding edges with 0)
-        if width != height:
-            square_size = min(width, height)
-            left = (width - square_size) / 2
-            top = (height - square_size) / 2
-            right = (width + square_size) / 2
-            bottom = (height + square_size) / 2
-            # Crop the center of the image
-            image = image.crop((left, top, right, bottom))
-        # now the image is square, resize it to be the right shape for the model input
-        if "Image" not in self.inputs:
-            raise ValueError("Couldn't find Image in model inputs - please report issue to Lobe!")
-        input_width, input_height = self.inputs["Image"]["shape"][1:3]
-        if image.width != input_width or image.height != input_height:
-            image = image.resize((input_width, input_height))
-        # make 0-1 float instead of 0-255 int (that PIL Image loads by default)
-        image = np.asarray(image) / 255.0
+
+        image = self.process_image(image, self.inputs.get("Image").get("shape"))
         # create the feed dictionary that is the input to the model
         # first, add our image to the dictionary (comes from our signature.json file)
         feed_dict = {self.inputs["Image"]["name"]: [image]}
@@ -70,7 +50,38 @@ class TFModel(object):
 
         # run the model! there will be as many outputs from session.run as you have in the fetches list
         outputs = self.session.run(fetches=[name for _, name in fetches], feed_dict=feed_dict)
+        return self.process_output(fetches, outputs)
+
+    def process_image(self, image, input_shape) -> np.ndarray:
+        """
+        Given a PIL Image, center square crop and resize to fit the expected model input, and convert from [0,255] to [0,1] values.
+        """
+        width, height = image.size
+        # ensure image type is compatible with model and convert if not
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+        # center crop image (you can substitute any other method to make a square image, such as just resizing or padding edges with 0)
+        if width != height:
+            square_size = min(width, height)
+            left = (width - square_size) / 2
+            top = (height - square_size) / 2
+            right = (width + square_size) / 2
+            bottom = (height + square_size) / 2
+            # Crop the center of the image
+            image = image.crop((left, top, right, bottom))
+        # now the image is square, resize it to be the right shape for the model input
+        input_width, input_height = input_shape[1:3]
+        if image.width != input_width or image.height != input_height:
+            image = image.resize((input_width, input_height))
+
+        # make 0-1 float instead of 0-255 int (that PIL Image loads by default)
+        image = np.asarray(image) / 255.0
+        # format input as model expects
+        return image.astype(np.float32)
+
+    def process_output(self, fetches, outputs) -> dict:
         # do a bit of postprocessing
+        out_keys = ["label", "confidence"]
         results = {}
         # since we actually ran on a batch of size 1, index out the items from the returned numpy arrays
         for i, (key, _) in enumerate(fetches):
@@ -78,16 +89,35 @@ class TFModel(object):
             if isinstance(val, bytes):
                 val = val.decode()
             results[key] = val
-        results["Labels"] = [list(x) for x in zip(self.labels, results["Confidences"])]
-        del results["Confidences"]
-        return results
+        confs = results["Confidences"]
+        labels = self.signature.get("classes").get("Label")
+        output = [dict(zip(out_keys, group)) for group in zip(labels, confs)]
+        sorted_output = {"predictions": sorted(output, key=lambda k: k["confidence"], reverse=True)}
+        return sorted_output
 
-    def cleanup(self):
+    def cleanup(self) -> None:
         # close our tensorflow session if one exists
         if self.session is not None:
             self.session.close()
             self.session = None
 
-    def __del__(self):
+    def __del__(self) -> None:
         self.cleanup()
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Predict a label for an image.")
+    parser.add_argument("image", help="Path to your image file.")
+    args = parser.parse_args()
+    # Assume model is in the parent directory for this file
+    model_dir = os.path.join(os.getcwd(), "..")
+
+    if os.path.isfile(args.image):
+        image = Image.open(args.image)
+        model_dir = os.path.join(os.getcwd(), "..")
+        model = TFModel(model_dir=model_dir)
+        model.load()
+        outputs = model.predict(image)
+        print(f"Predicted: {outputs}")
+    else:
+        print(f"Couldn't find image file {args.image}")
